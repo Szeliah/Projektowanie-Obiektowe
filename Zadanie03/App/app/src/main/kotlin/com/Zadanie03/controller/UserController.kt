827c02b9d4c79cde0d5963cbfc74d260bb12f15d
Szeliah <szeliszymon@gmail.com>
2026-04-07 18:18:12 +0200

    feat: added task 4.5

--- Zadanie03/App/app/src/main/kotlin/com/Zadanie03/controller/UserController.kt
+++ Zadanie03/App/app/src/main/kotlin/com/Zadanie03/controller/UserController.kt
@@ -5,17 +5,42 @@ import com.Zadanie03.model.LoginRequest
 import com.Zadanie03.service.AuthService
 
 import org.springframework.web.bind.annotation.*
+import org.springframework.http.ResponseEntity
+import org.springframework.http.HttpStatus
 
 @RestController
 class UserController(private val authService: AuthService) {
 
     @GetMapping("/users")
-    fun getUsers(): List<User> {
-        return authService.getAllUsers()
+    fun getUsers(): ResponseEntity<List<User>> {
+        if (authService.isLoggedIn()) {
+            return ResponseEntity.ok(authService.getUsers())
+        }
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null)
     }
 
     @PostMapping("/login") 
-    fun login(@RequestBody req: LoginRequest): Boolean {
-        return authService.authenticate(req.username, req.password)
+    fun login(@RequestBody req: LoginRequest): ResponseEntity<String> {
+        
+        val result = authService.authenticate(req.username, req.password)
+
+        if (result) {
+            return ResponseEntity.ok("You are logged in as ${req.username}")
+        } else {
+           return  ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Incorrect username or password")
+        }
+
     }
+
+    @GetMapping("/logout") 
+    fun logout(): ResponseEntity<String> {
+        
+        if (authService.isLoggedIn()) {
+            authService.logout()
+            return ResponseEntity.ok("Logged out")
+        }
+        return ResponseEntity.status(HttpStatus.CONFLICT).body("Already logged out")
+
+    }
+
 }
--- Zadanie03/App/app/src/main/kotlin/com/Zadanie03/service/AuthService.kt
+++ Zadanie03/App/app/src/main/kotlin/com/Zadanie03/service/AuthService.kt
@@ -6,17 +6,38 @@ import org.springframework.stereotype.Service
 
 @Service
 class AuthService {
+
     private val users = listOf(
         User("admin", "admin123", "Adam", 25),
         User("john", "password", "Jan", 35),
         User("ann", "1234", "Anna", 28)
     )
 
+    private var currentUser: String? = null
+
     fun authenticate(username: String, password: String): Boolean {
-        return users.any { it.username == username && it.password == password }
+        val result =  users.any { it.username == username && it.password == password }
+
+        if (result) {
+            currentUser = username
+            return true;
+        }
+        else {
+            currentUser = null
+            return false;
+        }
     }
 
-    fun getAllUsers(): List<User> {
+    fun getUsers(): List<User> {
         return users
     }
+
+    fun isLoggedIn(): Boolean {
+        return currentUser != null
+    }
+
+    fun logout() {
+        currentUser = null
+    }    
+
 }
